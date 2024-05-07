ba2b8ffb7884720e76a1a60051359c9ab4c5e379
Clara Quintela <claraquintela@gmail.com>
2024-05-07 09:25:19 -0400

    Ajout de la page de course (individuel)

--- claraquintela/resources/views/artist/course/class.blade.php
+++ claraquintela/resources/views/artist/course/class.blade.php
@@ -14,19 +14,10 @@
                             <img class="w-full h-full object-cover"
                                 src="https://cdn.pixabay.com/photo/2020/05/22/17/53/mockup-5206355_960_720.jpg"
                                 alt="Product Image">
+
+
                         </div>
-                        <div class="flex -mx-2 mb-4">
-                            <div class="w-1/2 px-2">
-                                <button
-                                    class="w-full bg-gray-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700">Add
-                                    to Cart</button>
-                            </div>
-                            <div class="w-1/2 px-2">
-                                <button
-                                    class="w-full bg-gray-200 dark:bg-gray-700 text-gray-800 dark:text-white py-2 px-4 rounded-full font-bold hover:bg-gray-300 dark:hover:bg-gray-600">Add
-                                    to Wishlist</button>
-                            </div>
-                        </div>
+
                     </div>
                     <div class="md:flex-1 px-4">
                         <h2 class="text-2xl font-bold text-gray-800 dark:text-white mb-2">{{ $course->name }}</h2>
@@ -46,14 +37,19 @@
                             <p class="text-gray-600 dark:text-gray-300 text-sm mt-2">
                                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
                                 sed ante justo. Integer euismod libero id mauris malesuada tincidunt. Vivamus commodo nulla
-                                ut
-                                lorem rhoncus aliquet. Duis dapibus augue vel ipsum pretium, et venenatis sem blandit.
-                                Quisque
-                                ut erat vitae nisi ultrices placerat non eget velit. Integer ornare mi sed ipsum lacinia,
+                                ut lorem rhoncus aliquet. Duis dapibus augue vel ipsum pretium, et venenatis sem blandit.
+                                Quisque ut erat vitae nisi ultrices placerat non eget velit. Integer ornare mi sed ipsum lacinia,
                                 non
                                 sagittis mauris blandit. Morbi fermentum libero vel nisl suscipit, nec tincidunt mi
                                 consectetur.
                             </p>
+                        </div>
+                        <div class="flex justify-center -mx-2 mb-4">
+                            <div class="w-center px-2">
+                                <button
+                                    class="w-full bg-gray-900 dark:bg-gray-600 text-white py-2 px-4 rounded-full font-bold hover:bg-gray-800 dark:hover:bg-gray-700">Add
+                                    to Cart</button>
+                            </div>
                         </div>
                     </div>
                 </div>
