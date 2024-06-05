f1a54c41400d8b211a514d504aa8de911d626150
Clara Quintela <claraquintela@gmail.com>
2024-06-05 08:11:39 -0400

    Correcting css (footer and nav)

--- claraquintela/resources/views/index.blade.php
+++ claraquintela/resources/views/index.blade.php
@@ -17,7 +17,7 @@
                     </div> --}}
                     <h1
                         class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white my-8">
-                        Stitches Bits</h1>
+                        Stitches & Bits</h1>
                     <p class="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
                         {{ __('index.main-text') }}
                     </p>
